21414c580daa5210af42a6a499593f2324421173
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2024-01-26 21:20:47 +0200

    Update dataTebleShowOrder.tsx
    add edit button and fix sort in id  date and noa

--- app/commponents/showOrder/dataTebleShowOrder.tsx
+++ app/commponents/showOrder/dataTebleShowOrder.tsx
@@ -6,6 +6,7 @@ import DataTable from "react-data-table-component";
 import {useRouter} from "next/navigation";
 import Swal from "sweetalert2";
 import {data, dataInOut, dataStoryType, items} from "@/app/type/typeDataFirebase";
+import Link from "next/link";
 
 const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
  {
@@ -15,35 +16,57 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
      typeOrder :string,
      id :string
  })=>{
+
     let rot = useRouter()
     const [isPending, setPending] = useState<boolean>(false);
     const [isTransitionStarted, startTransition] = useTransition();
     const isMutating = isPending || isTransitionStarted;
 
 
-    const customSortFunction = (rowA:any,rowB:any)=>{
-        if(rowA.sender){
-            const a = rowA.sender.toLowerCase()
-            const b = rowB.sender.toLowerCase()
-            return  a>b?1:b>a?-1:0
-        }else if(rowA.client){
-            const a = rowA.client.toLowerCase()
-            const b = rowB.client.toLowerCase()
-            return  a>b?1:b>a?-1:0
-        }else if(rowA.date){
-            const a = rowA.date
-            const b = rowB.date
-            return  a>b?1:b>a?-1:0
-        } else if(rowA.noa){
-            const a = rowA.client
-            const b = rowB.client
-            return  a>b?1:b>a?-1:0
-        }else if(rowA.id){
-            const a = rowA.client
-            const b = rowB.client
-            return  a>b?1:b>a?-1:0
+    const caseInsensitiveSortId = (rowA:data, rowB:data) => {
+        const a = rowA.id;
+        const b = rowB.id;
+
+        if (a > b) {
+            return 1;
         }
-    }
+
+        if (b > a) {
+            return -1;
+        }
+
+        return 0;
+    };
+    const caseInsensitiveSortNoa = (rowA:data, rowB:data) => {
+        const a = Number(rowA.noa);
+        const b = Number(rowB.noa);
+
+        if (a > b) {
+            return 1;
+        }
+
+        if (b > a) {
+            return -1;
+        }
+
+        return 0;
+    };
+    const caseInsensitiveSortDate = (rowA:data, rowB:data) => {
+        const a = rowA.date;
+        const b = rowB.date;
+
+        if (a > b) {
+            return 1;
+        }
+
+        if (b > a) {
+            return -1;
+        }
+
+        return 0;
+    };
+
+    const typeEditOrder:string = typeOrder == 'order'?'output':'input'
 
     const columns:any[] = [
         {
@@ -74,26 +97,22 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
         },
         {
             name:'Sender',
-            sortable:true,
             selector:(row:any)=>{
                 return(
                     <p style={row.delete?{color:'#ff5757'}:{color:'#fff'}}>{row.sender}</p>
                 )
             },
-            sortFunction:customSortFunction,
             style:{
                 paddingTop:"15px"
             }
         },
         {
             name:'Client',
-            sortable:true,
             selector:(row:any)=>{
                 return(
                     <p style={row.delete?{color:'#ff5757'}:{color:'#fff'}}>{row.client}</p>
                 )
             },
-            sortFunction:customSortFunction,
             style:{
                 paddingTop:"15px"
             }
@@ -106,7 +125,7 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
                     <p style={row.delete?{color:'#ff5757'}:{color:'#fff'}}>{row.date}</p>
                 )
             },
-            sortFunction:customSortFunction,
+            sortFunction:caseInsensitiveSortDate,
             style:{
                 paddingTop:"15px"
             }
@@ -119,7 +138,7 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
                     <p style={row.delete?{color:'#ff5757'}:{color:'#fff'}}>{row.noa}</p>
                 )
             },
-            sortFunction:customSortFunction,
+            sortFunction:caseInsensitiveSortNoa,
             style:{
                 paddingTop:"15px"
             }
@@ -132,11 +151,21 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
                     <p style={row.delete?{color:'#ff5757'}:{color:'#fff'}}>{row.id}</p>
                 )
             },
-            sortFunction:customSortFunction,
+            sortFunction:caseInsensitiveSortId,
             style:{
                 paddingTop:"15px"
             }
 
+        },
+        {
+            name:"Edite",
+            selector:(row:data)=>{
+                return(
+                    <Link href={`/pages/editOrder/${row.id}/${typeEditOrder}`} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" data-name="Layer 1" viewBox="0 0 24 24" id="Edit"><path d="M3.5,24h15A3.51,3.51,0,0,0,22,20.487V12.95a1,1,0,0,0-2,0v7.537A1.508,1.508,0,0,1,18.5,22H3.5A1.508,1.508,0,0,1,2,20.487V5.513A1.508,1.508,0,0,1,3.5,4H11a1,1,0,0,0,0-2H3.5A3.51,3.51,0,0,0,0,5.513V20.487A3.51,3.51,0,0,0,3.5,24Z" fill="#00c6ff" className="color000000 svgShape"></path><path d="M9.455,10.544l-.789,3.614a1,1,0,0,0,.271.921,1.038,1.038,0,0,0,.92.269l3.606-.791a1,1,0,0,0,.494-.271l9.114-9.114a3,3,0,0,0,0-4.243,3.07,3.07,0,0,0-4.242,0l-9.1,9.123A1,1,0,0,0,9.455,10.544Zm10.788-8.2a1.022,1.022,0,0,1,1.414,0,1.009,1.009,0,0,1,0,1.413l-.707.707L19.536,3.05Zm-8.9,8.914,6.774-6.791,1.4,1.407-6.777,6.793-1.795.394Z" fill="#00c6ff" className="color000000 svgShape"></path></svg>
+                    </Link>
+                )
+            }
         },
         {
             name:'Delete',
@@ -200,6 +229,7 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
     }
 
 
+
     let allOrder:dataInOut | []= []
     dataOrder?.forEach((e:data)=>{
         var arrayitem:items =[...e.items]
@@ -207,6 +237,8 @@ const DataTables = ({dataOrder,dataStore, coc,typeOrder,id}:
             if(f == id ){
                 // @ts-ignore
                 allOrder.push(e)
+            }else {
+
             }
         })
     })
