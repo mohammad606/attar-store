01c2524f56fa1778b17ee0dfbd7efefc6766bdbb
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2024-01-26 21:17:58 +0200

    Update removeUndfindFromArray.ts
    fix error object

--- app/hooks/removeUndfindFromArray.ts
+++ app/hooks/removeUndfindFromArray.ts
@@ -1,7 +1,13 @@
 
-
 export const RemoveUndefinedFromArray = (data:any)=>{
-    return  data.filter((item:any, index:number) => {
-        return Object.keys(item).length > 0 || index === 0;
-    });
+    if(Array.isArray(data)){
+        return  data.filter((item:any, index:number) => {
+            return Object.keys(item).length > 0 || index === 0;
+        });
+    }else {
+        const array =Object.values(data)
+        return  array.filter((item:any, index:number) => {
+            return Object.keys(item).length > 0 || index === 0;
+        });
+    }
 }
